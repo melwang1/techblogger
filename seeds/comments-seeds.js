236cb1bba21881e93ddfb683677cb86358d63281
Melanie Wang <melaniiewang@hotmail.com>
2023-06-11 12:02:30 -0400

    added user and comments seeds

---
+++ seeds/comments-seeds.js
@@ -0,0 +1,32 @@
+const { Comment } = require ('../models/comments');
+
+const commentData = [
+
+    {
+        comment_text: "These were amazing posts! Love to read these.",
+        post_id: 2,
+        user_id: 3
+    },
+
+    {
+        comment_text: "Thank you for sharing.",
+        post_id: 1,
+        user_id: 5
+    },
+
+    {
+        comment_text: "I never knew there was so much to coding!",
+        post_id: 3,
+        user_id: 2
+    },
+
+    {
+        comment_text: "I love the coding community and the value they bring into this world!",
+        post_id: 4,
+        user_id: 1
+    },
+]
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
---
+++ seeds/user-seeds.js
@@ -0,0 +1,34 @@
+const { User } = require('../models/user');
+
+const userData = [
+
+    {
+        name: "Paisley",
+        email: "paisleylee@hotmail.com",
+        passowrd: "flowergardene456"
+    },
+
+    {
+        name: "Sal",
+        email: "sal@hotmail.com",
+        password: "password12345"
+    },
+
+    {
+        name: "Lernantino",
+        email: "lernantino@gmail.com",
+        password: "password12345"
+    },
+
+    {
+        name: "Amiko",
+        email: "amiko2k20@aol.com",
+        password: "password12345"
+    },
+
+    {
+        username: "Josh",
+        email: "joshbyle@gmail.com",
+        password: "joshiscool1234"
+    }
+]
